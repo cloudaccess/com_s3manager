b97f689f108cd27d8225699221a3cfcbd7e26755
andrew <andrew@78dfc88f-9610-4d31-86ec-1f9932253477>
2009-08-27 16:42:49 +0000

    Added com_s3manager
    git-svn-id: https://cloudaccess.svn.beanstalkapp.com/components/com_s3manager@17 78dfc88f-9610-4d31-86ec-1f9932253477

---
+++ install.s3manager.php
@@ -0,0 +1,13 @@
+<?php 
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.filesystem.folder'); 
+jimport('joomla.filesystem.file'); 
+
+
+function com_install()
+  {
+
+  }
+?>
