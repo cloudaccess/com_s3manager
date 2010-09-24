016432b946ac331bd9faeaedc71242aabf1ccba5
GDR\! <gdr@go2.pl>
2010-09-24 10:32:39 +0200

    README added

--- release.sh
+++ release.sh
@@ -3,5 +3,6 @@ cp -R administrator release/
 cp -R component release/
 cp *.php release/
 cp *.xml release/
-tar -C release/ -zcf release.tar.gz .
+find release/ -name .git -exec rm -rf {} \;
+tar -C release/ -zcf com_s3manager_`date +"%Y-%m-%d"`.tar.gz .
 rm -rf release
